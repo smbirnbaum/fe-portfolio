3901cf853f39afedf1a500e52e453f3f33055374
korvmacka <shawnabirnbaum@email.arizona.edu>
2026-05-19 14:35:17 +0200

    Organized game links into dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,18 @@ function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/portfolio">Portfolio</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/game">Game</Link>
+
+        <div className="dropdown">
+          <button className="dropdown-button">
+            Game ▾
+          </button>
+
+          <div className="dropdown-menu">
+            <Link to="/game">Play Game</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
       </div>
     </nav>
   )
